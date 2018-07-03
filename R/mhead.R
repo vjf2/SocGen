c28d413b90be786be9f96c9defaf5d266590d220
Vivienne Foroughirad <foroughirad@gmail.com>
2018-07-03 16:39:41 -0400

    added n argument to mhead

--- R/mhead.R
+++ R/mhead.R
@@ -2,17 +2,18 @@
 #'
 #' This function allows you to view a preview of wide matrices and dataframes.
 #' @param x a matrix or dataframe
+#' @param n number of row or columns to display (default 10)
 #' @keywords matrix head
 #' @export
 #' @examples
 #' mhead()
 
 
-mhead<-function(x){
+mhead<-function(x, n=10){
   nr<-nrow(x)
   nc<-ncol(x)
-  ifelse(nr<=10, nr<-nr, nr<-10)
-  ifelse(nc<=10, nc<-nc, nc<-10)
+  ifelse(nr<=n, nr<-nr, nr<-n)
+  ifelse(nc<=n, nc<-nc, nc<-n)
   return(x[1:nr, 1:nc])
 
 
