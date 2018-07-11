8d9095cade709752661d5b01edf61c21ac9e6679
Vivienne Foroughirad <foroughirad@gmail.com>
2018-07-11 15:40:41 -0400

    added retainMissing and value.name options

--- R/mat2dat.R
+++ R/mat2dat.R
@@ -2,18 +2,24 @@
 #'
 #' This function converts a matrix to a 3-column dataframe with dimnames and values
 #' @param x a matrix
+#' @param value.name a character string, name of values column
+#' @param retainMissing logical, if TRUE retain empty cells as records in dataframe
 #' @keywords unmatrix
 #' @export
 #' @examples
 #' mat2dat()
 
 
-mat2dat<-function (x) {
+mat2dat<-function (x, value.name="values", retainMissing=FALSE) {
   rnames <- rownames(x)
   cnames <- colnames(x)
   nmat<-expand.grid(rnames, cnames, stringsAsFactors = FALSE)
   nmat$values<-c(x)
+  
+  if(!retainMissing){
   nmat<-nmat[complete.cases(nmat),]
-  names(nmat)<-c("ID1", "ID2", "values")
+  }
+
+  names(nmat)<-c("ID1", "ID2", value.name)
   return(nmat)
 }
