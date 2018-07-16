a48cf70600a1c07d6b37aa354fea8b66d6cb349f
Vivienne Foroughirad <foroughirad@gmail.com>
2018-07-16 19:57:37 -0400

    added diag option to dat2mat

--- R/dat2mat.R
+++ R/dat2mat.R
@@ -3,22 +3,34 @@
 #' This function converts 3-column dataframe such as produced with mat2dat back into a matrix with the IDs in the first two columns used as dimnames
 #' @param x a matrix
 #' @param forceSymmetric boolean, if matrix has a empty triangle, fill over the diagonal
+#' @param diag boolean, should a diagonal be added if absent?
 #' @details If value column is type numeric or integer the function will return a numeric matrix, otherwise a character matrix will be returned. 
 #' @keywords rematrix
 #' @export
 #' @examples
 #' mat2dat()
 
+#add diag option
 
-dat2mat<-function(data = data, forceSymmetric = FALSE){
+
+dat2mat<-function(data = data, forceSymmetric = FALSE, diag = FALSE){
   ro<-sort(unique(data[,1]))
   nro<-length(ro)
   co<-sort(unique(data[,2]))
   nco<-length(co)
   mat<-matrix(nrow=nro, ncol=nco)
   dimnames(mat)<-list(as.character(ro), as.character(co))
+  
+  if(diag)
+    {
+    rco<-as.character(unique(c(ro, co)))
+    nrco<-length(rco)
+    mat<-matrix(nrow=nrco, ncol=nrco)
+    dimnames(mat)<-list(rco, rco)
+    }
+  
   apply(data, 1, function(w) {mat[as.character(w[1]),
-                               as.character(w[2])]<<-w[[3]]})
+                               as.character(w[2])]<<-w[3]})
   if(forceSymmetric)
   {
     #check that one triangle is empty
@@ -34,6 +46,13 @@ dat2mat<-function(data = data, forceSymmetric = FALSE){
   
   if(class(data[,3]) %in% c("integer", "numeric")) {
     mode(mat)<-"numeric"
+  }
+  
+  #fix logicals
+  if(class(data[,3])=="logical") 
+  {
+    mat<-trimws(mat)
+    mode(mat)<-"logical"
   }
   
   return(mat)
