a1db00ce3cf4922c9ed330f1cd840a7ed2523c6a
Vivienne Foroughirad <foroughirad@gmail.com>
2018-10-26 13:22:31 -0400

    added Levy flight description

--- R/bounded_random_walk.R
+++ R/bounded_random_walk.R
@@ -1,6 +1,6 @@
 #' Create a bounded random walk
 #'
-#' This function creates a random walk bounded by a maximum distance from the starting position.
+#' This function creates a random or semi-random walk bounded by a maximum distance from the starting position.
 #' @param n.times integer, the number of steps to take (default 100)
 #' @param maxDist numeric, the maximum distance from starting posistion (default NA)
 #' @param start x and y starting position (default (0,0))
@@ -22,6 +22,7 @@ bounded_walk <- function(n.times=100,
   for (i in 1:n.times) {
     repeat {
       ## pick step sizes
+      ## pull steps from a heavy-tailed distribution for a Levy flight type movement pattern
       xi <- rnorm(1, 0, abs(rnorm(1, 0.2, 0.5)))
       yi <- rnorm(1, 0, abs(rnorm(1, 0.2, 0.5)))
       ## new candidate locations
