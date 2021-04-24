9063fdaad589bc97c6a29d9db4f5b08b73b9d18e
vjf2 <foroughirad@gmail.com>
2021-04-23 23:10:21 -0400

    change output variable names in fast random points

--- R/fast_random_points.R
+++ R/fast_random_points.R
@@ -9,19 +9,19 @@
 #' @keywords sample relocations
 #' @export
 #' @examples
-#' 
+#'
 
 
 
 fast_random_points <- function(probweights, nd, dc, dgdf, gridrad) {
-  
+
   daily_dolphins <- sample(names(probweights), size=nd, replace=FALSE, prob=probweights)
   df <- dgdf[,which(colnames(dgdf) %in% daily_dolphins), drop=FALSE]
   df[is.na(df)] <- 0
   cells <- apply(df, 2, function(x) sample(seq_len(nrow(df)), 1, prob=x))
   xy <- dc[cells,,drop=FALSE]
-  xy <- xy + c(runif(nd, -gridrad, gridrad), runif(nd, -gridrad, gridrad))  
-  res <- data.frame(xy, dolphin_id=names(cells))
+  xy <- xy + c(runif(nd, -gridrad, gridrad), runif(nd, -gridrad, gridrad))
+  res <- data.frame(x=xy[,1], y=xy[,2], id=names(cells))
   return(res)
-  
+
 }
